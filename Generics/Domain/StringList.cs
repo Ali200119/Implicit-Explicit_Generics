9fc6f0d8d066d4aaa7b2a4be576c4ec4221d184e
Ali200119 <aliit@code.edu.az>
2023-01-06 02:34:35 +0400

    StringList

---
+++ Generics/Domain/StringList.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain;
+public class StringList
+{
+    private string[] array;
+
+    public StringList()
+    {
+        array = new string[0];
+    }
+
+    public void Add(string str)
+    {
+        Array.Resize(ref array, array.Length + 1);
+        array[array.Length - 1] = str;
+    }
+
+    public void GetAll()
+    {
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Generics/Main/Program.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+
+StringList list = new StringList();
+
+list.Add("Ali");
+
+list.Add("Cavid");
+
+list.Add("Elcan");
+
+list.Add("Rustam");
+
+
+list.GetAll();
