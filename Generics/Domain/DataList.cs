1a11d2bac66688d070ac3976dfbd61f726483a82
Ali200119 <aliit@code.edu.az>
2023-01-06 04:15:20 +0400

    Generic

---
+++ Generics/Domain/DataList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Domain
+{
+	public class DataList<T>
+	{
+        private T[] array;
+
+        public DataList()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T data)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = data;
+        }
+
+        public void GetAll()
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Generics/Main/Program.cs
+++ Generics/Main/Program.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Domain;
 using Domain.Models;
 
 #region StringList
@@ -37,15 +39,64 @@ using Domain.Models;
 
 #region StudentList
 
-StudentList list = new StudentList();
+//StudentList list = new StudentList();
 
-list.Add(new Student { Id = 1, Name = "Ali" });
+//list.Add(new Student { Id = 1, Name = "Ali" });
 
-list.Add(new Student { Id = 2, Name = "Cavid" });
+//list.Add(new Student { Id = 2, Name = "Cavid" });
 
-list.Add(new Student { Id = 3, Name = "Elekber" });
+//list.Add(new Student { Id = 3, Name = "Elekber" });
 
 
-list.GetAll();
+//list.GetAll();
+
+#endregion
+
+#region Generic
+
+//DataList<string> stringList = new DataList<string>();
+
+//stringList.Add("Ali");
+
+//stringList.Add("Cavid");
+
+//stringList.Add("Elcan");
+
+//stringList.Add("Rustam");
+
+
+//stringList.GetAll();
+
+
+
+
+
+//DataList<int> intList = new DataList<int>();
+
+//intList.Add(1);
+
+//intList.Add(2);
+
+//intList.Add(3);
+
+//intList.Add(7);
+
+
+//intList.GetAll();
+
+
+
+
+
+//DataList<Student> studentList = new DataList<Student>();
+
+//studentList.Add(new Student { Id = 1, Name = "Ali" });
+
+//studentList.Add(new Student { Id = 2, Name = "Cavid" });
+
+//studentList.Add(new Student { Id = 3, Name = "Elekber" });
+
+
+//studentList.GetAll();
 
 #endregion
